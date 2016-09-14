355d57dc6828514546242cadc90a5247ef002337
Matt L <mattlebermann@gmail.com>
2016-09-14 15:29:41 -0400

    Test Data added

--- src/com/stem/neuralnetwork/Main.java
+++ src/com/stem/neuralnetwork/Main.java
@@ -1,16 +1,26 @@
 package com.stem.neuralnetwork;
 
+import java.util.zip.GZIPInputStream;
+
 import org.neuroph.core.Layer;
 import org.neuroph.core.Neuron;
+import org.neuroph.core.data.DataSet;
+import org.neuroph.core.data.DataSetRow;
 import org.neuroph.core.input.WeightedSum;
 import org.neuroph.core.transfer.Sigmoid;
-import org.neuroph.nnet.ConvolutionalNetwork;
+import org.neuroph.nnet.SupervisedHebbianNetwork;
+import org.neuroph.util.TransferFunctionType;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		ConvolutionalNetwork network = new ConvolutionalNetwork();
+		GZIPInputStream gzip;
+		
+		SupervisedHebbianNetwork network = new SupervisedHebbianNetwork(785, 10, TransferFunctionType.SIGMOID);
+		
+		DataSet data = new DataSet(785, 10);
+		data.addRow(new DataSetRow());
 		
 		for (int i = 0; i < 3; i++) {
 			Layer layer = new Layer();
